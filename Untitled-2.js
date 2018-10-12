81a4d236b4fc3b298719e46281aaf52a30d10bb9
Daniel Combs <danielcombs@Daniels-MacBook-Pro.local>
2018-10-12 14:37:18 -0500

    Finished Fiona

---
+++ Untitled-2.js
@@ -0,0 +1,51 @@
+var princessFiona={
+    // district component
+    {district="Doluo"}
+    // platform component
+    {Platform={
+        taxes= "Dont pay them!",
+        Jobs= "Protecting the magical critters of the Kingdom Doluo",
+        Infrastructure= {
+            healthCare =" Merlin heals everyone fo free!",
+            crimeEnforcement= "The turn Buckle!"
+        },
+        // URL component
+    donationUrl= {
+        url= "www.shrek.com"
+    },
+    // calander events
+    calanderEvents=[
+        {date= "4/20/18",
+        event= "Farquad short jokes"
+        },
+        {date= "4/21/18",
+        event= "Shooting the shit with shrek"
+        },
+        {date="4/22/18",
+        event=" Pin the tail on the donkey"},
+
+    ]}
+
+// volunteer component
+volunteerInformation=[
+     name= (nameblank),
+    address= (addressBlank),
+    email= (blankEmail),
+    phoneNumber= (blankPhonenumber),
+    availability= [1,2,3,4],
+    willingToDo= (willToDoAnswer)
+],
+
+ // biography component
+Biography = " www.Shrekfartedandicameout.com",
+// image component
+imageGallery= {
+    headShot="www.farquadsux.org",
+    pictureOfFamily="www.shrekshot.com",
+    pictureofConstituents="www.donkeylovesparfaits.com",
+},
+// mission statement component
+missionStatement= "To kill the 3 blind mice!",
+// voting component
+votingURL= "www.Shrekforfirstman.com"
+}
